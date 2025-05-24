2ef7ea36f9556b11186b758f17344be48423edfb
Steve S. <J294862@tafe.wa.edu.au>
2025-05-24 18:42:50 +0800

    feat:fixed header and input in place at top of page.

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import { first151Pokemon, getFullPokedexNumber } from '../utils'
+import {useState} from "react"
+import {first151Pokemon, getFullPokedexNumber} from "../utils"
 
-export const SideNav = ({ selectedPokemon, setSelectedPokemon, handleCloseMenu, showSideMenu }) => {
+export const SideNav = ({selectedPokemon, setSelectedPokemon, handleCloseMenu, showSideMenu}) => {
   const [searchValue, setSearchValue] = useState("")
   const filtered = first151Pokemon.filter((poke, pokeIndex) => {
     if (getFullPokedexNumber(pokeIndex).includes(searchValue)) return true
@@ -12,36 +12,40 @@ export const SideNav = ({ selectedPokemon, setSelectedPokemon, handleCloseMenu, 
 
   return (
     <nav className={" " + (!showSideMenu ? " open" : "")}>
-      <div className={"header " + (!showSideMenu ? " open" : "")}>
-        <button onClick={handleCloseMenu} className="open-nav-button">
-          <i className="fa-solid fa-arrow-left-long"></i>
-        </button>
-        <h1 className="text-gradient">Pokédex</h1>
-      </div>
-      <input
-        placeholder="E.g. 001 or Bulba..."
-        value={searchValue}
-        onChange={(e) => {
-          setSearchValue(e.target.value)
-        }}
-      />
-      {filtered.map((pokemon, pokemonIndex) => {
-        const truePokedexNumber = first151Pokemon.indexOf(pokemon)
-        return (
-          <button
-            onClick={() => {
-              setSelectedPokemon(truePokedexNumber)
-              handleCloseMenu()
-            }}
-            key={pokemonIndex}
-            className={
-              "nav-card " + (pokemonIndex === selectedPokemon ? " nav-card-selected" : " ")
-            }>
-            <p>{getFullPokedexNumber(truePokedexNumber)}</p>
-            <p>{pokemon}</p>
+      <div className="nav-fixed-header">
+        <div className={"header " + (!showSideMenu ? " open" : "")}>
+          <button onClick={handleCloseMenu} className="open-nav-button">
+            <i className="fa-solid fa-arrow-left-long"></i>
           </button>
-        )
-      })}
+          <h1 className="text-gradient">Pokédex</h1>
+        </div>
+        <input
+          placeholder="E.g. 001 or Bulba..."
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value)
+          }}
+        />
+      </div>
+      <div className="nav-scrollable-content">
+        {filtered.map((pokemon, pokemonIndex) => {
+          const truePokedexNumber = first151Pokemon.indexOf(pokemon)
+          return (
+            <button
+              onClick={() => {
+                setSelectedPokemon(truePokedexNumber)
+                handleCloseMenu()
+              }}
+              key={pokemonIndex}
+              className={
+                "nav-card " + (pokemonIndex === selectedPokemon ? " nav-card-selected" : " ")
+              }>
+              <p>{getFullPokedexNumber(truePokedexNumber)}</p>
+              <p>{pokemon}</p>
+            </button>
+          )
+        })}
+      </div>
     </nav>
   )
 }
